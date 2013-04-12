ff4967ab028ae00b4780702c683bc12ab5495bba
Nicolas Rubiano <nick.guitar.r@gmail.com>
2013-04-12 18:24:37 -0500

    Cambios a JSF

--- semillero/src/edu/unbosque/dao/FacultiesHome.java
+++ semillero/src/edu/unbosque/dao/FacultiesHome.java
@@ -1,17 +1,13 @@
 package edu.unbosque.dao;
 
-// Generated 8/04/2013 05:28:06 PM by Hibernate Tools 4.0.0
+// Generated 9/04/2013 07:02:30 PM by Hibernate Tools 4.0.0
 
 import java.util.List;
 import javax.naming.InitialContext;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.LockMode;
-import org.hibernate.Query;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
 
 import edu.unbosque.model.Faculties;
 import static org.hibernate.criterion.Example.create;
@@ -28,35 +24,24 @@ public class FacultiesHome {
 	private final SessionFactory sessionFactory = getSessionFactory();
 
 	protected SessionFactory getSessionFactory() {
-		try {			
-			return (SessionFactory) new Configuration().configure().buildSessionFactory();					
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
 		} catch (Exception e) {
 			log.error("Could not locate SessionFactory in JNDI", e);
 			throw new IllegalStateException(
-					"Could not locate SessionFactory in JNDI " + e.getMessage());
+					"Could not locate SessionFactory in JNDI");
 		}
 	}
 
 	public void persist(Faculties transientInstance) {
 		log.debug("persisting Faculties instance");
-		Transaction tx = null;
 		try {
-		
-			tx = sessionFactory.getCurrentSession().beginTransaction();
-			
 			sessionFactory.getCurrentSession().persist(transientInstance);
-			tx.commit();
 			log.debug("persist successful");
 		} catch (RuntimeException re) {
-		
-			if(tx != null){
-				tx.rollback();
-			}
-			
 			log.error("persist failed", re);
 			throw re;
-		} finally {			
-			sessionFactory.getCurrentSession().close();
 		}
 	}
 
@@ -122,36 +107,20 @@ public class FacultiesHome {
 			throw re;
 		}
 	}
-	
-	public List<Faculties> findAll() {
+
+	public List<Faculties> findByExample(Faculties instance) {
 		log.debug("finding Faculties instance by example");
-				
-		Transaction tx = null;
-		
-		try {			
-			tx = sessionFactory.getCurrentSession().beginTransaction();
-							
-			Query results = sessionFactory
-					.getCurrentSession().createQuery("from Faculties");
-			
-			List<Faculties> listResult = results.list();
-			
+		try {
+			List<Faculties> results = (List<Faculties>) sessionFactory
+					.getCurrentSession()
+					.createCriteria("edu.unbosque.dao.Faculties")
+					.add(create(instance)).list();
 			log.debug("find by example successful, result size: "
-					+ listResult.size());
-			
-			tx.commit();
-			
-			return listResult;
+					+ results.size());
+			return results;
 		} catch (RuntimeException re) {
-			
-			if(tx != null){
-				tx.rollback();
-			}
-			
 			log.error("find by example failed", re);
 			throw re;
-		} finally {			
-			sessionFactory.getCurrentSession().close();
 		}
 	}
 }
